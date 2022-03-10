8ad7a11973aefcf0818a7480cd2c5bb11be06df3
codewizz <c.chaiwat.ch@gmail.com>
2022-03-10 08:44:49 +0700

    add: readme, update: parameter

---
+++ README.md
@@ -0,0 +1,11 @@
+# GET method overview
+* Path parameters
+* Predefined values
+* Query parameters
+
+## Path parameters
+```python
+@app.get("/blog/{id}")
+def index(id):
+    return { "message": f"Blog with id {id}"}
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return { "message": "Hello World!"}
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return { "message": f"Blog with id {id}"}
