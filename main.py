3d2f6e3c7822aa1429ddd6516ae01f95cd9bcf81
codewizz <c.chaiwat.ch@gmail.com>
2022-03-12 10:05:23 +0700

    update: how to add summary and description

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ def index():
     return {"message": "Hello World!"}
 
 
-@app.get("/blog/all", tags=["blog"])
-def get_blog(page=1, page_size: Optional[int] = None):
+@app.get(
+    "/blog/all",
+    tags=["blog"],
+    summary="Retrieve all blogs",
+    description="This api call simulates refresh blog",
+)
+def get_all_blog(page=1, page_size: Optional[int] = None):
     return {"message": f"All {page_size} blogs on page {page}"}
 
 
@@ -20,6 +25,13 @@ def get_blog(page=1, page_size: Optional[int] = None):
 def get_comment(
     id: int, comment_id: int, valid: bool = True, username: Optional[str] = None
 ):
+    """
+    Simulates retrieving a comment of a blog
+    - **id** mandatory path parameter
+    - **comment_id** optional query parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {
         "message": f"blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}"
     }
