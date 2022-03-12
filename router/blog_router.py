f3c3997e01202ff710925c774914bfd9e4b10b23
codewizz <c.chaiwat.ch@gmail.com>
2022-03-12 11:16:24 +0700

    add: blog route, update: restructure

---
+++ router/blog_router.py
@@ -0,0 +1,55 @@
+from enum import Enum
+from typing import Optional
+from fastapi import APIRouter, Response, status
+
+router = APIRouter(prefix="/blog", tags=["blog"])
+
+
+@router.get(
+    "/all",
+    summary="Retrieve all blogs",
+    description="This api call simulates refresh blog",
+    response_description="The list of available blogs",
+)
+def get_all_blog(page=1, page_size: Optional[int] = None):
+    return {"message": f"All {page_size} blogs on page {page}"}
+
+
+@router.get("/{id}/comments/{comment_id}", tags=["comment"])
+def get_comment(
+    id: int, comment_id: int, valid: bool = True, username: Optional[str] = None
+):
+    """
+    Simulates retrieving a comment of a blog
+    - **id** mandatory path parameter
+    - **comment_id** optional query parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
+    return {
+        "message": f"blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}"
+    }
+
+
+class BlogType(str, Enum):
+    short = "short"
+    story = "story"
+    howto = "howto"
+
+
+@router.get("/type/{type}")
+def get_blog_type(type: BlogType):
+    return {"message": f"Blog type {type}"}
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": f"blog {id} not found"}
+    return {"message": f"Blog with id {id}"}
+
+
+@router.post("/")
+def create_blog():
+    pass
