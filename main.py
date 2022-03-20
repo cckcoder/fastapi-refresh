eedac2f3c11c84033474f52436c3b756e10f052e
codewizz <c.chaiwat.ch@gmail.com>
2022-03-20 11:04:56 +0700

    update: custom exception

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status, HTTPException
+from fastapi.responses import JSONResponse, PlainTextResponse
 from db import user_model
 from db.database import engine
 
 from routers import user_router, blog_router, article_router
+from utils.exceptions import StoryException
 
 app = FastAPI()
 app.include_router(user_router.router)
@@ -13,6 +15,17 @@ app.include_router(blog_router.router)
 @app.get("/")
 def index():
     return {"message": "Hello World!"}
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=status.HTTP_418_IM_A_TEAPOT,
+        content={"detail": exc.name}
+    )
+
+@app.exception_handler(HTTPException)
+def custom_handler(request: Request, exc: StoryException):
+    return PlainTextResponse(str(exc), status_code=status.HTTP_400_BAD_REQUEST)
 
 
 user_model.Base.metadata.create_all(engine)
