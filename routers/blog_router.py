888aa124f16751df47eb22c94cc985820b85594c
codewizz <c.chaiwat.ch@gmail.com>
2022-03-23 21:47:07 +0700

    add: response status 200

--- routers/blog_router.py
+++ routers/blog_router.py
@@ -56,7 +56,9 @@ def get_blog(id: int, response: Response):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"error": f"blog {id} not found"}
-    return {"message": f"Blog with id {id}"}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"message": f"Blog with id {id}"}
 
 
 class Image(BaseModel):
